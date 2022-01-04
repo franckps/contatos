c68b131678f64aeb8e6c2ccf39ff1c3dc3a3783c
Francisco Pereira <fran.p.s@hotmail.com>
2022-01-04 14:51:48 -0300

    feat: add the find contact local database adapter implementation

--- src/backend/adapters/db/local/contact-local.js
+++ src/backend/adapters/db/local/contact-local.js
@@ -8,4 +8,8 @@ module.exports = class AddContactLocal {
     this.databaseInstance.push(data);
     Promise.resolve();
   }
+
+  async find() {
+    Promise.resolve(this.databaseInstance.map());
+  }
 };
