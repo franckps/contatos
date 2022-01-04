27f7ecbca5416ddf27856bdc707534c755e1bada
Francisco Pereira <fran.p.s@hotmail.com>
2022-01-04 15:47:39 -0300

    feat: add the index contact route

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -1,6 +1,7 @@
 const { Router, json } = require('express');
 const adaptRoute = require('./adapters/express/route-adapter');
 const makeAddContactController = require('./main/factories/controllers/addContact/addContact');
+const makeIndexContactController = require('./main/factories/controllers/indexContact/indexContact');
 const backend = Router();
 
 backend.use(json());
@@ -8,5 +9,6 @@ backend.use(json());
 const db = [];
 
 backend.post('/', adaptRoute(makeAddContactController(db)));
+backend.get('/', adaptRoute(makeIndexContactController(db)));
 
 module.exports = backend;
