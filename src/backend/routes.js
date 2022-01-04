ee4f8253f80d16994e060c0f7469bd4dcc3b9c52
Francisco Pereira <fran.p.s@hotmail.com>
2022-01-04 14:22:22 -0300

    feat: add the addContact route

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -1,6 +1,12 @@
 const { Router, json } = require('express');
+const adaptRoute = require('./adapters/express/route-adapter');
+const makeAddContactController = require('./main/factories/controllers/addContact');
 const backend = Router();
 
 backend.use(json());
+
+const db = [];
+
+backend.post('/', adaptRoute(makeAddContactController(db)));
 
 module.exports = backend;
