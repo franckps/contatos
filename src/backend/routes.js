0bc3cb573f636aec79f8410aef0ea815cf6cd471
Francisco Pereira <fran.p.s@hotmail.com>
2022-01-04 22:40:50 -0300

    feat: add the update contact route

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -2,6 +2,7 @@ const { Router, json } = require('express');
 const adaptRoute = require('./adapters/express/route-adapter');
 const makeAddContactController = require('./main/factories/controllers/addContact/addContact');
 const makeIndexContactController = require('./main/factories/controllers/indexContact/indexContact');
+const makeUpdateContactController = require('./main/factories/controllers/updateContact/updateContact');
 const backend = Router();
 
 backend.use(json());
@@ -10,5 +11,6 @@ const db = [];
 
 backend.post('/', adaptRoute(makeAddContactController(db)));
 backend.get('/', adaptRoute(makeIndexContactController(db)));
+backend.put('/:id', adaptRoute(makeUpdateContactController(db)));
 
 module.exports = backend;
