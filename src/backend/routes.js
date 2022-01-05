01f5792e156b7f020d88a5cffe0c32d9d9476039
Francisco Pereira <fran.p.s@hotmail.com>
2022-01-04 23:02:30 -0300

    feat: add the delete contact route

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -3,6 +3,7 @@ const adaptRoute = require('./adapters/express/route-adapter');
 const makeAddContactController = require('./main/factories/controllers/addContact/addContact');
 const makeIndexContactController = require('./main/factories/controllers/indexContact/indexContact');
 const makeUpdateContactController = require('./main/factories/controllers/updateContact/updateContact');
+const makeDeleteContactController = require('./main/factories/controllers/deleteContact/deleteContact');
 const backend = Router();
 
 backend.use(json());
@@ -12,5 +13,6 @@ const db = [];
 backend.post('/', adaptRoute(makeAddContactController(db)));
 backend.get('/', adaptRoute(makeIndexContactController(db)));
 backend.put('/:id', adaptRoute(makeUpdateContactController(db)));
+backend.delete('/:id', adaptRoute(makeDeleteContactController(db)));
 
 module.exports = backend;
