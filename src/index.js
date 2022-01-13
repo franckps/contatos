08e8fec65d68707923088541b71fee620966c03a
Francisco Pereira <fran.p.s@hotmail.com>
2022-01-12 23:22:39 -0300

    feat: add cors to allow the acess by other apps

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const { port } = require('./configs');
 const backendRoutes = require('./backend/routes');
 const frontendRoutes = require('./frontend/routes');
 
 const app = express();
+app.use(cors());
 
 app.use('/public', express.static(path.join(__dirname, 'frontend', 'public')));
 
@@ -15,5 +17,5 @@ app.set('views', path.resolve(__dirname, 'frontend', 'views'));
 app.use('/', frontendRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${ port }`)
+  console.log(`Server is running on port ${port}`);
 });
